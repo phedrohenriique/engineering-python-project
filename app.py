ba6db557da5abd04da461c00beb332afd772c946
phenn <phd19994@gmail.com>
2023-02-22 17:31:12 -0300

    feature: test-pyvista

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 # run pipenv shell to start shell
 # run exit in the activatetd shell
-print('Container Working with Python')
-print('Conteainer Updated')
+import numpy as np
+import pyvista as pv
+
+points = [-1, 1, -4, 4, -1, 1]
+mesh = pv.Box(points)
+mesh.plot(show_edges=True)
+
+# plotter = pv.Plotter()
+# plotter.add_mesh(mesh, show_edges=True)
+# plotter.show()
+
+print("Container Working with Python")
+print("Conteainer Updated")
